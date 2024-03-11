42def6f87e01882094576e18eea85e99f4143e21
giorgi3011 <giorgitsibliashvili386@gmail.com>
2024-03-11 19:51:53 +0400

    coomit

---
+++ day 6/homework/lesson 6.py
@@ -0,0 +1,84 @@
+#homework first
+
+print(40<4000 or 10<11)
+print(30<20 or 20>10)
+print(80>40 or 300>1)
+print(200>50 or 50>20)
+print(50<49 or 40>42)
+
+#homework second
+
+print(50>60 and 100<111)
+print(60<500 and 40>9)
+print(100<502 and 2>0)
+print(70<50 and 20>21)
+print(41>40 and 36<38)
+
+#homework third
+
+verible_first=(300>526 or 50<2) #false
+print(verible_first)
+veraible_second=(20>19 and 80<82) #true
+print(veraible_second)
+
+#homework fourth
+
+verible_first= input("შემოიტანეთ პირველი რიცხი:")
+veraible_second=input("შემოიტანეთ მეორე რიცხვი:")
+print(verible_first>veraible_second or veraible_second>verible_first)
+print(veraible_second<verible_first or verible_first<veraible_second)
+
+#homework fifth
+
+verible_first=(300>10)
+veraible_second=(29<3)
+veraible_third=(veraible_second<verible_first)
+print(veraible_third)
+
+
+
+#first homework
+
+chad=True
+first="giorgi"
+second=22.6
+third=80
+
+print=(type(chad))
+print=(type(first))
+print=(type(second))
+print=(type(third))
+
+#second homework
+
+name=input("please enter your name :")
+experiense=input("how long have you been studing in goa  ")
+print(" wonderful" +" " + "name" + " " + "good lock") 
+
+#third homework
+
+verible_first=(25/5)
+print(verible_first)
+print(type(verible_first))
+
+#fourfh homework
+
+number1=30
+number2=30
+print(str(number1)+str(number2))
+
+#fifth homework
+
+num1=10.5
+num2=20.9
+num3=5.5
+num4=2.9
+num5=1.5
+
+print(str(num1))
+print(str(num2))
+print(str(num3))
+print(str(num4))
+print(str(num5))
+
+
