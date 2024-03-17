0111597d31b1b1bbf3ce36612a2906a99c38fbb6
giorgi3011 <giorgitsibliashvili386@gmail.com>
2024-03-17 20:19:01 +0400

    coomit

---
+++ day 7/homework/lesson 7.py
@@ -0,0 +1,34 @@
+#first task
+
+print(input("please enter your name:"))
+print("name")
+
+print(input("please enter your age:"))
+print("name")
+
+print(input("enter the answer: 9/2=)"))
+
+print(input("goa is best:"))
+
+#second task
+
+print(10<11)
+print(20>12)
+print(50>49)
+print(2<3)
+print(54>52)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
