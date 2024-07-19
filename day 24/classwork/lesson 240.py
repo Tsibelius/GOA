2879f99cb1c30e6d215a1734c6d8016b91e804af
giorgi3011 <giorgitsibliashvili386@gmail.com>
2024-07-19 22:42:59 +0400

    'commit'

---
+++ day 24/classwork/lesson 240.py
@@ -0,0 +1,8 @@
+array = [1 , 2 , 3 , 4 , 5 ]
+
+print(array[4])
+
+
+array = [1 , 2 , 3 , 4 , 5]
+array.extend([6 , 7 , 8])
+print(array)
---
+++ day 24/homework/lesson 24.py
@@ -0,0 +1,16 @@
+chad_list = [] 
+for i in range(1, 11): chad_list.append(i) 
+print(chad_list)
+
+
+even_num =[2, 4, 6, 8, 10]
+odd_num =(1, 3, 5, 7, 9)
+
+even_num.extend(odd_num)
+print(even_num)
+
+
+tventy_chad =list(range(1,21))
+
+for chad in tventy_chad:
+    print(chad)
