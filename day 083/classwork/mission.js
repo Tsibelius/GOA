af134d01c1f3d02ed5ba24562bb14871e38a0d1c
giorgi3011 <giorgitsibliashvili386.gmail.com>
2025-09-28 00:45:39 +0300

    'commit'

---
+++ day 083/classwork/mission.js
@@ -0,0 +1,9 @@
+const myName = "გიორგი";
+
+const userName = prompt("შეიყვანეთ თქვენი სახელი:");
+
+if (userName === myName) {
+  console.log(" ვა სეხნიები ვართ ");
+} else {
+  console.log("შენ  სხვა სახელი გქვია მე სხვა");
+}
