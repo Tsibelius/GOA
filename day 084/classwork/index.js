a8dd7e7e3eb38aac84991c7aa726dbd46d148287
Tsibelius <tsibelius276@gmail.com>
2025-10-10 11:55:22 +0300

    index.js

---
+++ day 084/classwork/index.js
@@ -0,0 +1,9 @@
+let myAnimal = "wolf"; 
+
+let userAnimal = prompt("Enter an animal:");
+
+if (userAnimal === myAnimal || userAnimal === "lomi") {
+  console.log("you entered my favourite animal or king of animals");
+} else {
+  console.log("You entered the animal i hate");
+}
