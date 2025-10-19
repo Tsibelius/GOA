b649469054679720908bf8ea2b954799c4b56da1
giorgi3011 <giorgitsibliashvili386.gmail.com>
2025-10-19 08:59:15 +0300

    'commit'

--- day 084/classwork/index.js
+++ day 084/classwork/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let myAnimal = "wolf"; 
 
 let userAnimal = prompt("Enter an animal:");
@@ -7,3 +8,14 @@ if (userAnimal === myAnimal || userAnimal === "lomi") {
 } else {
   console.log("You entered the animal i hate");
 }
+=======
+let myAnimal = "wolf"; 
+
+let userAnimal = prompt("Enter an animal:");
+
+if (userAnimal === myAnimal || userAnimal === "lomi") {
+  console.log("you entered my favourite animal or king of animals");
+} else {
+  console.log("You entered the animal i hate");
+}
+>>>>>>> 3cb2b4a ('commit')
--- day 084/classwork/index1.js
+++ day 084/classwork/index1.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let myName = "Giorgi";
 
 let userName = prompt("Enter your name:");
@@ -11,3 +12,18 @@ if (userName === myName && userAge > 18) {
   console.log("dzia ra arseba xar ");
   console.log("dedamiweli xar saertod?")
 }
+=======
+let myName = "Giorgi";
+
+let userName = prompt("Enter your name:");
+let userAge = Number(prompt("Enter your age:")); 
+
+if (userName === myName && userAge > 18) {
+  console.log("we have same name and you are more than 18 yo");
+} else if (userName !== myName && userAge > 18) {
+  console.log("We do not have same names but you are more than 18 yo");
+} else {
+  console.log("dzia ra arseba xar ");
+  console.log("dedamiweli xar saertod?")
+}
+>>>>>>> 3cb2b4a ('commit')
