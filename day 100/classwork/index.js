4fa97593d51190bac99246d6344066757c7d7a1c
giorgi3011 <giorgitsibliashvili386.gmail.com>
2026-01-18 23:13:31 +0300

    'commit'

---
+++ day 100/classwork/index.js
@@ -0,0 +1,11 @@
+/* საკლასო დავალებად codewars გვქონდა და ბავშვებმა თითოეული ამოცანა გუნდურად ამოვხსენით */
+
+
+//                                               დავალება 1-ლი
+
+//  https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/train/javascript
+
+
+//                                               დავალება მე-2
+
+//  https://www.codewars.com/kata/54da5a58ea159efa38000836/train/javascript
