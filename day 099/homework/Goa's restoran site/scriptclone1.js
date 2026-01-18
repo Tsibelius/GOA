a2989a796c3647344df2d329f25e2164341d375a
giorgi3011 <giorgitsibliashvili386.gmail.com>
2026-01-18 14:13:07 +0300

    'commit'

---
+++ day 099/homework/Goa's restoran site/scriptclone1.js
@@ -0,0 +1,2 @@
+let name = prompt("Please enter your name:");
+prompt("Welcome, Mr. " + " " + name + " " + ". To view our delicious menu, please complete the registration form.");
---
+++ day 099/homework/Goa's restoran site/sqriptclone1.js
@@ -0,0 +1,4 @@
+let name = prompt("Please enter your name:");
+prompt("Welcome, Mr. " + " " + name + " " + ". How can I assist you? Today, I will be your host.");
+alert("Our restaurant's menu includes delicious dishes, drinks, and desserts. Please complete the registration form to view our menu and enjoy special offers!");
+alert("To register, please click on our website's logo.");
